3d08de3b4acede857c47a736b68b677f08de4b3f
MatiasLLL <matias.larralde97@gmail.com>
2023-07-19 18:15:26 +0200

    Implementation de la gestion de téléchargement de fichier

--- routes/book.js
+++ routes/book.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-mixed-spaces-and-tabs */
 const express = require("express");
-const auth = require("../middleware/auth");
 const router = express.Router();
+
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 const bookControllers = require("../controllers/book");
 
 router.get("/", auth, bookControllers.getAllBooks);
-router.post("/", auth, bookControllers.createBook);
+router.post("/", auth, multer, bookControllers.createBook);
 router.get("/:id", auth, bookControllers.getOneBook);
-router.put("/:id", auth, bookControllers.modifyBook);
+router.put("/:id", auth, multer, bookControllers.modifyBook);
 router.delete("/:id", auth, bookControllers.removeBook);
 
 module.exports = router;
