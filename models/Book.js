4ea0a1be28e840c0148a868a7e94985adce2100e
MatiasLLL <matias.larralde97@gmail.com>
2023-07-19 18:26:17 +0200

    Modification models Book

--- models/Book.js
+++ models/Book.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const bookSchema = mongoose.Schema({
 	book: { type: Array , default : [{
+		id: { type: String, required: true },
 		_id: { type: String, required: true },
 		userId: { type: String, required: true },
 		title: { type: String, required: true },
