289efa72fa8af8d26b79f5d32e45eb830675e8af
MatiasLLL <matias.larralde97@gmail.com>
2023-07-19 15:28:43 +0200

    Mise en place d'un systeme d'authentification

--- routes/book.js
+++ routes/book.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-mixed-spaces-and-tabs */
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
 const bookControllers = require("../controllers/book");
 
-router.post("/", bookControllers.createBook);
-router.put("/:id", bookControllers.modifyBook);
-router.delete("/:id", bookControllers.removeBook);
-router.get("/:id", bookControllers.getOneBook);
-router.get("/", bookControllers.getAllBooks);
+router.get("/", auth, bookControllers.getAllBooks);
+router.post("/", auth, bookControllers.createBook);
+router.get("/:id", auth, bookControllers.getOneBook);
+router.put("/:id", auth, bookControllers.modifyBook);
+router.delete("/:id", auth, bookControllers.removeBook);
 
 module.exports = router;
